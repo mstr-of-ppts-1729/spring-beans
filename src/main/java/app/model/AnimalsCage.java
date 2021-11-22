175dd5121e8ba7e7a47970d5c239881f4893b36d
mstr-of-ppts-1729 <mstr-of-ppts-1729@yandex.ru>
2021-11-23 00:32:47 +0300

    Initial commit

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,27 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+    @Autowired
+    private Timer nanoTime;
+
+    public Timer getTimer() {
+        return nanoTime;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(nanoTime.getTime());
         System.out.println("________________________");
     }
 }
